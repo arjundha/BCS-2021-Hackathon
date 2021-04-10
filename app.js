10404e453d69638185722dea216e418aca463d08
Arjun <59514930+arjundha@users.noreply.github.com>
2021-04-10 15:39:40 -0700

    ejs time muahhaa

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const express = require("express");
 const app = express();
 
-// const path = require('path');
-// const session = require("express-session");
+const path = require('path');
+const session = require("express-session");
 
+app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static("public"));
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+
+// ------------------------- //
+//    SESSIONS + COOKIES     //
+// ------------------------- //
+app.use(session({
+	name: "idk",
+	secret: "secret",
+	resave: true,
+	saveUninitialized: true,
+}));
+
+
+//
 // ROUTES TO PAGES //
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html')
+    res.render('index')}
+    )
+
+app.get('/form', function(req, res) {
+    res.render('form', {
+        name: "Kyla"
+    })
   })
-
-
   
+
+
 var port = process.env.PORT || 8080;
 
 app.listen(port);
